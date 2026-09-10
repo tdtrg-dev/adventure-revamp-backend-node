5e026274363f9f23a83f0f77c6de9e5d5987cf60
alifaraztdtrg-933 <alifaraz.tdtrg@gmail.com>
2026-09-11 02:36:00 +0500

    cehck

--- src/services/adminAuth.service.js
+++ src/services/adminAuth.service.js
@@ -9,11 +9,11 @@ function fail(message, code = 400) {
 }
 
 async function login(email, password) {
-  const admin = await Admin.findOne({ email }).select('+password');
-  // if (!admin) throw fail('Invalid password.1');
+  const admin = await Admin.findOne({ email }).select('password');
+  if (!admin) throw fail('Invalid password.1');
 
-  // if (!admin.is_active) throw fail('Your account has been deactivated. Please contact support.', 403);
-  // if (!(await bcrypt.compare(password, admin.password))) throw fail('Invalid password.2');
+  if (!admin.is_active) throw fail('Your account has been deactivated. Please contact support.', 403);
+  if (!(await bcrypt.compare(password, admin.password))) throw fail('Invalid password.2');
 
   const accessToken = await issueToken(admin._id, 'admin');
   return { admin, accessToken };
